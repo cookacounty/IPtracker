0fa6fd9701d0f0debd02fc27f4183b0258e099c7
Aaron Cook <cookacounty@gmail.com>
2013-08-10 11:45:15 -0400

    Start of ch5 in Rails tutorial

---
+++ Rails/IPtracker/app/models/micropost.rb
@@ -0,0 +1,6 @@
+class Micropost < ActiveRecord::Base
+  
+  belongs_to :user
+  
+  validates :conetent, :length => { :maximum => 140 }
+end
---
+++ Rails/IPtracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IPtracker::Application.config.secret_key_base = '5141117711588c094f276d129d857ad27c86737a0c27266c8520fdf8bab9a47b9e51d6fc52d59a376b3f908e63880bb79b77096f0f041dce6600ad04c8105ee6'
