3ceacd4fe3f38e0275ac724343afab82b26ca580
Aaron Cook <cookacounty@gmail.com>
2013-08-11 19:36:32 -0400

    User signin finished ch 8

--- Rails/IPtracker/spec/models/user_spec.rb
+++ Rails/IPtracker/spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
-      password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
@@ -88,6 +89,16 @@ describe User do
     end
   end
   
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -96,5 +107,10 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
